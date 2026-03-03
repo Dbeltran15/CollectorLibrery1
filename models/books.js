491187e0f9a4cd99bded7f84b229fec681613cb8
Dbeltran15 <61804197+Dbeltran15@users.noreply.github.com>
2026-03-02 18:31:14 -0600

    migrations ande relations files

--- models/books.js
+++ models/books.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Books.belongsTo(models.Autores, { foreignKey: 'AuthorID' });
+      //Books.hasOne(models.readingStatus{{foreignKey: 'bookId'}})
     }
   }
   Books.init({
